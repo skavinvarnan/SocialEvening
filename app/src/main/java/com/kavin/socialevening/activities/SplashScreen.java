5f00b52feff7fbde2d5953f24ef30fdb08d3ff26
Kavin Varnan <skavinvarnan@gmail.com>
2015-10-29 00:33:33 +0530

    Initial Commit

---
+++ app/src/main/java/com/kavin/socialevening/activities/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.kavin.socialevening.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kavin.socialevening.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
